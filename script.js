d10a171bc5f69de03bcf9656323045e920cfb86b
Antriksh Jain <antriksh30jaina@gmail.com>
2023-05-31 10:31:18 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dark mode toggle functionality
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
+
+console.log('Custom JavaScript code');
